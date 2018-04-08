73ff5cd342a2ba9d6a1d2c9d3c96aa7d2929fbc9
Luoshipeng <strangermy98@gmail.com>
2018-04-09 03:12:51 +0800

    完善QQBar进度条圆环，增加用户体验。

--- app/src/main/java/com/yibao/music/view/MusicProgressView.java
+++ app/src/main/java/com/yibao/music/view/MusicProgressView.java
@@ -4,14 +4,17 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PointF;
 import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.yibao.music.R;
+import com.yibao.music.util.LogUtil;
 
 
 /**
@@ -30,7 +33,89 @@ public class MusicProgressView
     private int mMax = 100;
     private int mProgress;
     private RectF mOval;
+    private RectF rectFRing;
     private Paint mPaint;
+    private Paint grayPaint;
+    /**
+     *
+     */
+    private boolean useCenter = false;
+    private float startAngle = -90;
+
+    public MusicProgressView(Context context) {
+        this(context, null);
+
+    }
+
+    private void init() {
+        int strokeWidth = 9;
+        grayPaint = new Paint();
+        grayPaint.setColor(Color.parseColor("#ccdcdbdb"));
+        grayPaint.setStyle(Paint.Style.STROKE);
+        grayPaint.setAntiAlias(true);
+        grayPaint.setStrokeWidth(strokeWidth);
+
+        mPaint = new Paint();
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(strokeWidth);
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(Color.parseColor("#E64040"));
+
+
+    }
+
+    public MusicProgressView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        //挂载布局
+
+        View view = View.inflate(context, R.layout.music_progress_bar, this);
+        mIvIcon = view.findViewById(R.id.music_progress_bar);
+        init();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    /**
+     *  在ViewGroup上绘制东西的时候往往重写的是dispatchDraw()
+     * @param canvas
+     */
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        //绘制图标和文字
+        super.dispatchDraw(canvas);
+        int bottom = mIvIcon.getBottom();
+        int top = mIvIcon.getTop();
+        int left = mIvIcon.getLeft();
+        int right = mIvIcon.getRight();
+        drawGrayRing(canvas, left, top, right, bottom);
+        drawProgress(canvas, left, top, right, bottom);
+    }
+
+    private void drawGrayRing(Canvas canvas, int left, int top, int right, int bottom) {
+        if (rectFRing == null) {
+
+            rectFRing = new RectF(left, top, right, bottom);
+        }
+
+        float sweepAngle = mMax * 1.0f / mMax * 360;
+        canvas.drawArc(rectFRing, startAngle, sweepAngle, useCenter, grayPaint);
+
+    }
+
+    private void drawProgress(Canvas canvas, int left, int top, int right, int bottom) {
+        if (isProgressEnable) {
+            if (mOval == null) {
+                mOval = new RectF(left, top, right, bottom);
+            }
+            float sweepAngle = mProgress * 1.0f / mMax * 360;
+            canvas.drawArc(mOval, startAngle, sweepAngle, useCenter, mPaint);
+        }
+
+    }
+
 
     /**
      * 设置是否允许进度
@@ -74,52 +159,5 @@ public class MusicProgressView
      */
     public void setNote(String content) {
         mTvNote.setText(content);
-    }
-
-    public MusicProgressView(Context context) {
-        this(context, null);
-    }
-
-    public MusicProgressView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        //挂载布局
-
-        View view = View.inflate(context, R.layout.music_progress_bar, this);
-        mIvIcon = view.findViewById(R.id.music_progress_bar);
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-    }
-
-    @Override
-    protected void dispatchDraw(Canvas canvas) {
-        //绘制图标和文字
-        super.dispatchDraw(canvas);
-        int bottom = mIvIcon.getBottom();
-        int top = mIvIcon.getTop();
-        int left = mIvIcon.getLeft();
-        int right = mIvIcon.getRight();
-        if (isProgressEnable) {
-            if (mOval == null) {
-
-                mOval = new RectF(left, top, right, bottom);
-
-            }
-            float startAngle = -90;
-            float sweepAngle = mProgress * 1.0f / mMax * 360;
-            //是否以图片的中心为圆点
-            boolean useCenter = false;
-            if (mPaint == null) {
-                mPaint = new Paint();
-                mPaint.setStyle(Paint.Style.STROKE);
-                mPaint.setStrokeWidth(9);
-                mPaint.setAntiAlias(true);
-                mPaint.setColor(Color.parseColor("#E64040"));
-            }
-            canvas.drawArc(mOval, startAngle, sweepAngle, useCenter, mPaint);
-        }
-
     }
 }
